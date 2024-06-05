d20b31e9a93c9f2f8e4357f26700cdf5c387921b
KodyStudio <justinsilvac360@gmail.com>
2024-06-05 00:50:38 -0600

    reverse commit

---
+++ Backend/src/test/java/com/s15_16_n_java/rest_service/RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s15_16_n_java.rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Front End/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:'https://justin-campos.github.io/S15-16-n-java/dist',
+  server: {
+    host: process.env.VITE_HOST || 'localhost',
+  },
+})
